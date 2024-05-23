4f0329b4574a9889fef9fcacb2050b56b2c352d2
Wondo Jung <yarang@gmail.com>
2024-05-23 14:23:47 +0900

    인증 메일의 홈페이지 링크 삭제

--- internal/html_msg.py
+++ internal/html_msg.py
@@ -20,7 +20,6 @@ def auth_msg(msg_link):
                     <p style="font-size: 24px; margin-top: 20px; color: #555;">
                         이메일 인증을 완료하고 CYRUS를 사용하려면 아래 버튼을 클릭해주세요.
                     </p>
-                    <a href='{msg_link}' style="text-decoration: none; background-color: #007bff; color: #fff; padding: 10px 20px; font-size: 20px; border-radius: 5px; margin-top: 20px; display: inline-block;">이메일 인증하기</a>
                 </td>
             </tr>
         </table>
@@ -41,7 +40,6 @@ def html_ok_msg(web_url):
                 <td align="center" style="background-color: #f2f2f2; padding: 20px;">
                     <h1 style="font-size: 64px; color: #333;">Certified</h1>
                     <h2 style="font-size: 24px; margin-top: 20px; color: #555;">이메일이 인증 되었습니다.</h2>
-                    <a href='{web_url}' style="text-decoration: none; color: #007bff; cursor: pointer; margin-top: 20px;">Go Cyrus</a>
                 </td>
             </tr>
         </table>
